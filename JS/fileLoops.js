bf4500372daa0d826313ccfe2880e370aca629f1
Faieq Rahman <faieqrahman@gmail.com>
2023-09-01 13:54:01 +0100

    fileLoops html and js added

---
+++ JS/fileLoops.js
@@ -0,0 +1,13 @@
+for(i=1;i<10;i++){
+    console.log(i);
+}
+
+x=2
+loopCounter=0
+while(x<10000){
+    x=x*x;
+    loopCounter++;
+    console.log("x is equal to "+ x)
+    console.log("loopCounter is equal to " + loopCounter)
+
+}
